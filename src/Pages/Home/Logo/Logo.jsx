6d1eeecf01f274ced8c5079337c139801a0e7e4a
Muzbah Uddin <rimonmr444@gmail.com>
2023-11-25 22:24:27 +0600

    create logo section in home page

--- src/Pages/Home/Logo/Logo.jsx
+++ src/Pages/Home/Logo/Logo.jsx
@@ -8,8 +8,9 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 const Logo = () => {
     return (
-        <div>
+        <div className='h-[200px] bg-gray-300 flex items-center'>
             <Swiper
+                slidesPerView={4}
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
@@ -23,15 +24,33 @@ const Logo = () => {
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src='https://www.vectorlogo.zone/logos/gartner/gartner-ar21.svg' alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://vectorlogoseek.com/wp-content/uploads/2019/11/getapp-vector-logo.png" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://cdn-bkknn.nitrocdn.com/fGcPNFYNoLNWGSllWzgDxtBAbrFDaIYE/assets/images/optimized/rev-3b327ff/www.caspio.com/wp-content/uploads/2021/01/gartner.png.webp" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://cdn-bkknn.nitrocdn.com/fGcPNFYNoLNWGSllWzgDxtBAbrFDaIYE/assets/images/optimized/rev-3b327ff/www.caspio.com/wp-content/uploads/2022/02/g2-best-software-2022-badge-it-infrastructure.svg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://www.tmgm.co.nz/img/main-logo-white.png" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://cdn-bkknn.nitrocdn.com/fGcPNFYNoLNWGSllWzgDxtBAbrFDaIYE/assets/images/optimized/rev-3b327ff/www.caspio.com/wp-content/uploads/2022/01/getapp-2021.png.webp" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://cdn-bkknn.nitrocdn.com/fGcPNFYNoLNWGSllWzgDxtBAbrFDaIYE/assets/images/optimized/rev-3b327ff/www.caspio.com/wp-content/uploads/2022/02/users-love-us.svg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://cdn-bkknn.nitrocdn.com/fGcPNFYNoLNWGSllWzgDxtBAbrFDaIYE/assets/images/optimized/rev-3b327ff/www.caspio.com/wp-content/uploads/2022/01/aws-advanced-technology-partner.svg" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='h-[70px]' src="https://cdn-bkknn.nitrocdn.com/fGcPNFYNoLNWGSllWzgDxtBAbrFDaIYE/assets/images/optimized/rev-3b327ff/www.caspio.com/wp-content/uploads/2022/02/g2-best-software-2022-badge-development.svg" alt="" />
+                </SwiperSlide>
             </Swiper>
 
         </div>
