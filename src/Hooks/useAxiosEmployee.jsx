71d35c5be0b03fb44c3b05413b78c13a0c0e6e6e
Muzbah Uddin <rimonmr444@gmail.com>
2023-12-19 15:24:28 +0600

    updated some changes

--- src/Hooks/useAxiosEmployee.jsx
+++ src/Hooks/useAxiosEmployee.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import useAuth from "./useAuth";
+// import { useNavigate } from "react-router-dom";
+// import useAuth from "./useAuth";
 
 const axiosEmployee = axios.create({
     baseURL: 'https://asset-management-server-one.vercel.app',
@@ -9,30 +9,30 @@ const axiosEmployee = axios.create({
 })
 
 const useAxiosEmployee = () => {
-    const navigate = useNavigate()
-    const { logout } = useAuth()
+    // const navigate = useNavigate()
+    // const { logout } = useAuth()
 
-    axiosEmployee.interceptors.request.use(function (config) {
-        const token = localStorage.getItem('access_token')
-        // console.log('request stopped by interceptors', token);
-        config.headers.authorization = `Bearer ${token}`
-        return config;
-    }, function (error) {
-        // Do something with request error
-        return Promise.reject(error);
-    });
+    // axiosEmployee.interceptors.request.use(function (config) {
+    //     const token = localStorage.getItem('access_token')
+    //     // console.log('request stopped by interceptors', token);
+    //     config.headers.authorization = `Bearer ${token}`
+    //     return config;
+    // }, function (error) {
+    //     // Do something with request error
+    //     return Promise.reject(error);
+    // });
 
-    //interceptors response
-    axiosEmployee.interceptors.response.use(function (response) {
-        return response
-    }, async (error) => {
-        const status = error.response.status;
-        if (status === 401 || status === 403) {
-            await logout();
-            navigate('/login')
-        }
-        return Promise.reject(error);
-    });
+    // //interceptors response
+    // axiosEmployee.interceptors.response.use(function (response) {
+    //     return response
+    // }, async (error) => {
+    //     const status = error.response.status;
+    //     if (status === 401 || status === 403) {
+    //         await logout();
+    //         navigate('/login')
+    //     }
+    //     return Promise.reject(error);
+    // });
 
     return axiosEmployee
 };
